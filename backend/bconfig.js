381afac6f1c9d65e34a3316d06ba1f161cbe5baa
aishwarya-drishti <aishwaryagupta720@gmail.com>
2024-12-21 20:18:25 +0530

    Updated readme

--- backend/bconfig.js
+++ backend/bconfig.js
@@ -4,7 +4,7 @@ const bconfig = {
     database: {
         host: 'app-demo.cv0gka4gicz4.us-east-2.rds.amazonaws.com',
         user: 'admin',
-        password: 'm>Vtl_M0l*(0o_i$pZAYbokM>O1Z',
+        password: 'veryfakepassword',
         database: 'MyAppDB'
     },
     server: {
